dae7ca13a48c9b3eb88102cbc6a820fc9bb0d48b
João Vinícus <joaoviniciusferdlbqt@gmail.com>
2025-10-25 00:00:59 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio';
+import { ProdutoDetalheComponent } from './produto-detalhe/produto-detalhe';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'produto/:id', component: ProdutoDetalheComponent }
+];
